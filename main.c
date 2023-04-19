0ee883f62f93631a49bf0f8daef741beaf7e33cb
Melina <mrequena@fue.undef.edu.ar>
2023-04-19 15:03:33 -0300

    Ej 8 :)

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
+/* Guia 0,  Ej 8
+ * A través de una función que reciba por referencia un número entero, calcular el
+cubo de dicho número y mostrarlo en el bloque principal de ejecución del
+programa. */
 
-int main() {
-    printf("Hello, World!\n");
+#include<stdio.h>
+int cube(int *);
+
+int main(){
+    int num, *numptr, numcube;
+    printf("Ingrese un numero\n");
+    scanf("%d", &num);
+    numptr = &num;
+    numcube = cube(numptr);
+    printf("El numero %d elevado al cubo es %d", num, numcube);
+
     return 0;
+}
+
+int cube(int *numptr){
+    return *numptr * *numptr * *numptr;
 }
